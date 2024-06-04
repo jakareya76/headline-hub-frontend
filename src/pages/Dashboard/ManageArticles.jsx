95a197607164b1b97c9099a920c9c2e0fbc8e01f
jakareya76 <jakareya1306@gmail.com>
2024-06-04 16:14:46 +0600

    create aprove and delete for article

--- src/pages/Dashboard/ManageArticles.jsx
+++ src/pages/Dashboard/ManageArticles.jsx
@@ -3,8 +3,6 @@ import Swal from "sweetalert2";
 import { toast } from "react-toastify";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 
-import { MdDelete } from "react-icons/md";
-
 const ManageArticles = () => {
   const axiosSecure = useAxiosSecure();
 
@@ -15,6 +13,64 @@ const ManageArticles = () => {
       return res.data;
     },
   });
+
+  const handleApprove = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Approve it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.patch(`/approve-news/${id}`);
+
+        if (res.data.modifiedCount > 0) {
+          refetch();
+          Swal.fire({
+            title: "Completed!",
+            text: "News Has Been Approve.",
+            icon: "success",
+          });
+        }
+      }
+    });
+  };
+
+  const handleDecline = (id) => {
+    console.log(id);
+  };
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/news/${id}`);
+
+        if (res.data.deletedCount > 0) {
+          refetch();
+          Swal.fire({
+            title: "Deleted!",
+            text: "news has been deleted.",
+            icon: "success",
+          });
+        }
+      }
+    });
+  };
+
+  const handleMakePremium = (id) => {
+    console.log(id);
+  };
 
   return (
     <div className="py-5">
@@ -42,7 +98,7 @@ const ManageArticles = () => {
             </tr>
           </thead>
           <tbody>
-            {articles.map((article, idx) => {
+            {articles.map((article) => {
               const isPending = article.status === "pending" ? true : false;
 
               return (
@@ -65,19 +121,31 @@ const ManageArticles = () => {
                   <th className="grid grid-cols-3 gap-1">
                     {isPending ? (
                       <>
-                        <button className="col-span-2 text-white btn btn-sm btn-success ">
+                        <button
+                          onClick={() => handleApprove(article._id)}
+                          className="col-span-2 text-white btn btn-sm btn-success "
+                        >
                           approve
                         </button>
-                        <button className="text-white btn btn-sm btn-warning ">
+                        <button
+                          onClick={() => handleDecline(article._id)}
+                          className="text-white btn btn-sm btn-warning "
+                        >
                           decline
                         </button>
                       </>
                     ) : (
                       <>
-                        <button className="flex col-span-2 text-xs text-white btn btn-sm btn-primary ">
+                        <button
+                          onClick={() => handleMakePremium(article._id)}
+                          className="flex col-span-2 text-xs text-white btn btn-sm btn-primary "
+                        >
                           make premium
                         </button>
-                        <button className="text-white btn btn-sm btn-error ">
+                        <button
+                          onClick={() => handleDelete(article._id)}
+                          className="text-white btn btn-sm btn-error "
+                        >
                           Delete
                         </button>
                       </>
